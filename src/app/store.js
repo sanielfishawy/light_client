2987033690617a6973358885dfce888b5e2c6913
Sani Elfishawy <elfishawy.sani@gmail.com>
2020-11-19 09:56:21 -0800

    working with light_server

--- src/app/store.js
+++ src/app/store.js
@@ -11,4 +11,4 @@ export const store = configureStore({
 //   async () => {await store.dispatch(fetchWooferState())},
 //   500,
 // )
-  // (async () => {await store.dispatch(fetchWooferState())})()
+(async () => {await store.dispatch(fetchLightState())})()
